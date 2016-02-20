a54e8626c6b9d36a14b1692bd6bf06bdee0c7d9d
Sylvia Ma <xiangwma@umich.edu>
2016-02-19 21:18:36 -0600

    i dont'

--- quiz.js
+++ quiz.js
@@ -1,26 +1,37 @@
-//This is the function to draw one line of star.
-function print(j){
+//This is the function to draw the j order line of star. Height is the total height of the tree, and j is the j(order) line.
+function print(j, height){
   var phrase = "";
+
+//This is for the space on the left.
+  for (i=0; i<=height-j;i+=1){
+  	phrase += " ";
+  }
+
+//This is for the star.
   for (i=0; i<2*j-1;i+=1){
     phrase+= "p";
   }
-  document.getElementById("here").innerHTML+= phrase + "<br>";
+
+//This is for the space on the right.
+  for (i=0; i<=height-j;i+=1){
+  	phrase += " ";
+  }
+
+  var output = phrase.replace(/p/g, document.getElementById("mark").value)
+  console.log(output);
 }
 
-//This is the function to draw multiple lines.
+//This is the function to draw multiple lines. Second
 function draw(height){
   for (j=1; j<=height;j+=1){
-    print(j);
+    print(j, height);
   }
 }
 
+//trigger this function first.
 function btn(){
 var height = document.getElementById("height").value;
 draw(height);
-
-var Rep = document.getElementById("here").innerHTML;
-var newRep= Rep.replace(/p/g, document.getElementById("mark").value);
-document.getElementById("here").innerHTML= newRep;
 }
 
 
